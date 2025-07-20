e971e9e63c8d098dfd3631c0a3ad804f63cc1b6a
Nicholas <83509008+Stnkybo@users.noreply.github.com>
2025-07-20 20:23:30 +0800

    Fixed block retrival from negative coordinates caused by importer modulo calculation

---
+++ src/engine/math/mod.h
@@ -0,0 +1,12 @@
+//
+// Created by Lamad on 20/07/2025.
+//
+
+#ifndef MOD_H
+#define MOD_H
+
+inline int mod(const int a, const int b) {
+    return (a % b + b) % b;
+}
+
+#endif //MOD_H
--- src/engine/terrain/world.cpp
+++ src/engine/terrain/world.cpp
@@ -4,6 +4,7 @@
 
 #include "world.h"
 #include "chunk.h"
+#include "../math/mod.h"
 
 
 Voxel* World::getBlock(int x, int y, int z) {
@@ -12,10 +13,9 @@ Voxel* World::getBlock(int x, int y, int z) {
     //get chunk if it exists
     Chunk* chunk = getChunk(chunk_coord);
     if (chunk != nullptr) {
-        int local_x, local_y, local_z;
-        local_x = x % CHUNK_SIZE_X;
-        local_y = y % CHUNK_SIZE_Y;
-        local_z = z % CHUNK_SIZE_Z;
+        int local_x = mod(x,CHUNK_SIZE_X);
+        int local_y = mod(y,CHUNK_SIZE_Y);
+        int local_z = mod(z,CHUNK_SIZE_Z);
         return &chunk->at(local_x,local_y,local_z);
     }
     // if no chunk data exist
