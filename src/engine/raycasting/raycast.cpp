99035e45c3781c359d022dc6b6360ba28ab73f8a
Nicholas <nicholasdannolfo@gmail.com>
2026-05-08 12:03:14 +0800

    Added core directiores and files for raycast

---
+++ src/engine/raycasting/raycast.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nikolas on 5/8/26.
+//
+
+#include "raycast.h"
---
+++ src/engine/raycasting/raycast.h
@@ -0,0 +1,13 @@
+//
+// Created by nikolas on 5/8/26.
+//
+
+#ifndef SDL3PRJ_RAYCAST_H
+#define SDL3PRJ_RAYCAST_H
+
+
+class raycast {
+};
+
+
+#endif //SDL3PRJ_RAYCAST_H
