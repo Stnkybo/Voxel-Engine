676ebc5da8dcf6dd0a74a94f6785195664533f3c
Nicholas <83509008+Stnkybo@users.noreply.github.com>
2026-05-27 12:31:18 +0800

    reimplemented vcpkg support because its actually good

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,12 +1,6 @@
 # cmake/deps.cmake
 
 set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}" CACHE STRING "" FORCE)
-include(FetchContent)
-
-# Always download dependencies into a global cache so they're reused across builds
-if(NOT DEFINED FETCHCONTENT_BASE_DIR)
-    set(FETCHCONTENT_BASE_DIR "${CMAKE_SOURCE_DIR}/.deps")
-endif()
 
 # -----------------------------
 # OpenGL
@@ -21,86 +15,30 @@ endif()
 # -----------------------------
 # SDL3
 # -----------------------------
-FetchContent_Declare(
-    SDL3
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG release-3.2.18
-    GIT_SHALLOW TRUE
-)
-FetchContent_MakeAvailable(SDL3)
+find_package(SDL3 CONFIG REQUIRED)
 
 # -----------------------------
 # Glad
 # -----------------------------
-set(GLAD_GLAPI_EXPORT OFF CACHE BOOL "" FORCE)
-FetchContent_Declare(
-    glad
-    GIT_REPOSITORY https://github.com/Dav1dde/glad.git
-    GIT_TAG v0.1.36
-    GIT_SHALLOW TRUE
-)
-FetchContent_MakeAvailable(glad)
+find_package(glad CONFIG REQUIRED)
 
 # -----------------------------
 # GLM
 # -----------------------------
-FetchContent_Declare(
-    glm
-    GIT_REPOSITORY https://github.com/g-truc/glm.git
-    GIT_TAG 1.0.1
-    GIT_SHALLOW TRUE
-)
-FetchContent_MakeAvailable(glm)
+find_package(glm CONFIG REQUIRED)
 
 # -----------------------------
 # Assimp
 # -----------------------------
-set(ASSIMP_WARNINGS_AS_ERRORS OFF CACHE BOOL "" FORCE)
-set(ASSIMP_BUILD_ZLIB ON CACHE BOOL "" FORCE)
-FetchContent_Declare(
-    assimp
-    GIT_REPOSITORY https://github.com/assimp/assimp.git
-    GIT_TAG v5.4.3
-    GIT_SHALLOW TRUE
-)
-FetchContent_MakeAvailable(assimp)
+find_package(assimp CONFIG REQUIRED)
 
 # -----------------------------
 # ImGui
 # -----------------------------
-FetchContent_Declare(
-    imgui
-    GIT_REPOSITORY https://github.com/ocornut/imgui.git
-    GIT_TAG v1.91.6
-    GIT_SHALLOW TRUE
-)
-FetchContent_GetProperties(imgui)
-if(NOT imgui_POPULATED)
-    FetchContent_MakeAvailable(imgui)
-    add_library(imgui STATIC
-        ${imgui_SOURCE_DIR}/imgui.cpp
-        ${imgui_SOURCE_DIR}/imgui_draw.cpp
-        ${imgui_SOURCE_DIR}/imgui_widgets.cpp
-        ${imgui_SOURCE_DIR}/imgui_tables.cpp
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl3.cpp
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
-    )
-    target_include_directories(imgui SYSTEM PUBLIC
-        ${imgui_SOURCE_DIR}
-        ${imgui_SOURCE_DIR}/backends
-    )
-    target_link_libraries(imgui PRIVATE SDL3::SDL3 glad)
-endif()
+find_package(imgui CONFIG REQUIRED)
 
 # -----------------------------
 # stb_image
 # -----------------------------
-FetchContent_Declare(
-    stb
-    GIT_REPOSITORY https://github.com/nothings/stb.git
-    GIT_TAG master
-    GIT_SHALLOW TRUE
-)
-FetchContent_MakeAvailable(stb)
-add_library(stb_image INTERFACE)
-target_include_directories(stb_image SYSTEM INTERFACE ${stb_SOURCE_DIR})
+find_package(stb REQUIRED)
+target_include_directories(${PROJECT_NAME} PRIVATE ${stb_INCLUDE_DIR})
